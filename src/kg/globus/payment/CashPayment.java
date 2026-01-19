27be337f518239d4db986b20fbfe8a4bed4600b9
Ali <@.com>
2026-01-19 18:04:25 +0600

    ad

--- src/kg/globus/payment/CashPayment.java
+++ src/kg/globus/payment/CashPayment.java
@@ -3,5 +3,6 @@ package kg.globus.payment;
 public class CashPayment {
     public static void main(String[] args) {
         System.out.println("Paying with cash");
+        System.out.println("Paying with cashes");
     }
 }
