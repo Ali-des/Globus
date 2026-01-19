51240de80430b2f99abb53f32755ba5fe1a3edd4
Ali <@.com>
2026-01-19 17:45:50 +0600

    Second bonus

--- src/kg/globus/payment/Bonus.java
+++ src/kg/globus/payment/Bonus.java
@@ -3,5 +3,6 @@ package kg.globus.payment;
 public class Bonus {
     public static void main(String[] args) {
         System.out.println("Discount with bonus");
+        System.out.println("Discount with bonus");
     }
 }
