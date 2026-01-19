895a0c972fdbc125506f7ea22e339e05f01a966e
Ali <@.com>
2026-01-19 19:00:38 +0600

    helloworld

--- src/kg/globus/payment/CashPayment.java
+++ src/kg/globus/payment/CashPayment.java
@@ -4,5 +4,6 @@ public class CashPayment {
     public static void main(String[] args) {
         System.out.println("Paying with cash");
         System.out.println("Hello world");
+        System.out.println("Hello worlds");
     }
 }
